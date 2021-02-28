35340e26469cb0a83e2b5369acc95853c9b60ba6
Jordy Pouw <jordypouw@gmail.com>
2021-02-28 22:55:15 +0100

    position waves

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 import { myself } from "./myself";
+import { wave4, wave5, wave6 } from "./waves";
 
-document.body.innerHTML = myself();
+document.body.innerHTML = myself() + wave6() + wave5() + wave4();
