e19d6443bb3942a7318f853f6b0d1c286ff60e35
Jordy Pouw <jordypouw@gmail.com>
2021-03-01 22:17:13 +0100

    thunder

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,11 @@ import { myself } from "./myself";
 import { wave4, wave5, wave6 } from "./waves";
 
 document.body.innerHTML = myself() + wave6() + wave5() + wave4();
+
+const waves = document.querySelectorAll(".wave");
+
+setInterval(() => {
+  waves.forEach((wave) =>
+    wave.style.setProperty("--animation-time", Math.random() * 3 + 1 + "s")
+  );
+}, 4000);
