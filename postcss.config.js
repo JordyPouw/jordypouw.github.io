abeaad26d3f65d76dd1d4a20f40d59c838aaa5fd
Jordy Pouw <jordypouw@gmail.com>
2021-03-04 22:43:35 +0100

    let snowpack do css import for ya

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = (ctx) => ({
   map: ctx.options.map,
   parser: ctx.options.parser,
   plugins: {
-    "postcss-import": {},
     "postcss-units": { size: 16 },
     "postcss-preset-env": {
       stage: 0,
