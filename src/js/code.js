8c5b50004579ba391e088bba2747337658fdd7bf
Jordy Pouw <jordypouw@gmail.com>
2021-03-13 16:34:10 +0100

    updating code flow

---
+++ src/js/code.js
@@ -0,0 +1,163 @@
+import { typewriter } from "./typewriter";
+
+// init.
+const codelines = document.querySelector(".js-code-lines");
+const input = document.querySelector(".js-code-input");
+const prefix = "dude";
+input.focus();
+
+const opthelp = ["--help", "--h"];
+const optwho = ["whoyou", "who are you"];
+const optwhat = ["whatyoudo", "got some projects"];
+const optdm = ["dmme", "lemme slide in your dm"];
+const optsurp = ["surprise", "gimme"];
+
+// input handler.
+const cmd = (value) => {
+  const command = [
+    ...opthelp,
+    ...optwho,
+    ...optwhat,
+    ...optdm,
+    ...optsurp,
+  ].find((opt) => `${prefix} ${opt}` === value);
+
+  if (opthelp.includes(command)) help();
+  if (optwho.includes(command)) whoyou();
+  if (optwhat.includes(command)) whatyoudo();
+  if (optdm.includes(command)) dmme();
+  if (optsurp.includes(command)) surprise();
+};
+
+input.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    const value = input.value;
+    input.value = "";
+    codelines.innerHTML += `<span class="user-input">${value}</span>`;
+    cmd(value);
+  }
+});
+
+// observer.
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach(() => {
+    codelines.scrollTop = codelines.scrollHeight;
+  });
+});
+
+observer.observe(codelines, { childList: true });
+
+// run first command.
+setTimeout(
+  () =>
+    typewriter({
+      elem: input,
+      sentences: ["dude --help"],
+      minSpeed: 50,
+      maxSpeed: 100,
+      onEnd: () => {
+        setTimeout(() => {
+          input.value = "";
+          input.blur();
+          help();
+        }, 1000);
+      },
+    }),
+  2000
+);
+
+const help = () =>
+  typewriter({
+    elem: codelines,
+    sentences: [
+      "How dis work?",
+      "  dude [command] [flags]",
+      " ",
+      "All of the 1 flags:",
+      `  ${opthelp.join(", ")}`,
+      " ",
+      "Commands:",
+      `  - ${optwho.join(", ")}`,
+      `  - ${optwhat.join(", ")}`,
+      `  - ${optdm.join(", ")}`,
+      `  - ${optsurp.join(", ")}`,
+    ],
+    lineTag: "p",
+    minSpeed: 1,
+    maxSpeed: 10,
+    onEnd: () => {
+      input.focus();
+    },
+  });
+
+const whoyou = () =>
+  typewriter({
+    elem: codelines,
+    sentences: [
+      "cd projects/jordypouw.github.io/",
+      "........ ✓",
+      "Name: Jordy Pouw",
+      "Date of birth: unknown",
+      "Location: The Netherlands",
+      "Profession: Lets code do brrrr",
+      "ERROR",
+      "ERROR",
+      "Github: https://github.com/jordypouw",
+      "Socially awkward: ☑",
+      "Ability to fly: ☒",
+      "Traveled through time: ☑",
+      "CLASSIFIED",
+      "CLASSIFIED",
+      "CLASSIFIED",
+      "Program terminated",
+      "....................",
+      "Protocol breached",
+      "Activate self destruction",
+      ".......... 3",
+      ".......... 2",
+      ".......... 1",
+      "Gotcha :')",
+    ],
+    lineTag: "p",
+    minSpeed: 50,
+    maxSpeed: 100,
+    onEnd: () => {
+      input.focus();
+    },
+  });
+
+const whatyoudo = () =>
+  typewriter({
+    elem: codelines,
+    sentences: ["Ehhh.. working on this 1"],
+    lineTag: "p",
+    minSpeed: 50,
+    maxSpeed: 100,
+    onEnd: () => {
+      input.focus();
+    },
+  });
+
+const dmme = () =>
+  typewriter({
+    elem: codelines,
+    sentences: ["Ehhh.. working on this 2"],
+    lineTag: "p",
+    minSpeed: 50,
+    maxSpeed: 100,
+    onEnd: () => {
+      input.focus();
+    },
+  });
+
+const surprise = () =>
+  typewriter({
+    elem: codelines,
+    sentences: ["Ehhh.. working on this 3"],
+    lineTag: "p",
+    minSpeed: 50,
+    maxSpeed: 100,
+    onEnd: () => {
+      input.focus();
+    },
+  });
